bad6d04fb0e1f482af0c5b9e0356baea187759e2
Irish Moore <jasonamoore@frontier.com>
2022-03-06 16:40:29 -0500

    update api files to hopefully login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -110,9 +111,7 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
-
-  // pass in req.body instead to only update what's passed through
+  
   User.update(req.body, {
     individualHooks: true,
     where: {
