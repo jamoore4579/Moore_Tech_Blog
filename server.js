d3d6e5cde5f08abc5268471f3a92ea24d21c6a34
Irish Moore <jasonamoore@frontier.com>
2022-03-07 15:46:00 -0500

    built README and Heroku upload

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: { originalMaxAge: 60000},
+  cookie: { originalMaxAge: 600000},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
